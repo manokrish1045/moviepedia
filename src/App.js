84717953e636c88b7f139dd22a802c61a1850228
manokrish2000 <manokrish104525@gmail.com>
2023-03-16 19:35:42 +0530

    1st

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // const names = ["Mano", "Vijay", "Virat", "Sharuk"]
+  const users = [
+    {
+      name: 'Mano',
+      pic: "https://www.creative-tim.com/blog/content/images/2022/01/which-development-job-is-right-for-you.jpg"
+    },
+    {
+      name: 'vijay',
+      pic: "https://m.media-amazon.com/images/M/MV5BZWJlODhlMzctOTU0Yi00MTUwLTkxODYtMDNjNTQxYTI2YTE1XkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg"
+    },
+    {
+      name: 'virat',
+      pic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiAoDwXfflPgO00fa31FNaW4BKAKA9uSB8Zw&usqp=CAU"
+    }
+  ]
+  // const name = "Mano"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>Hello {name}</h1>
+      <label htmlFor='username'>name : </label>
+      <input id="username" placeholder='enter name' /> */}
+
+      {/* <Msg name='Mano' pic="https://www.creative-tim.com/blog/content/images/2022/01/which-development-job-is-right-for-you.jpg" />
+      <Msg name='vijay' pic="https://m.media-amazon.com/images/M/MV5BZWJlODhlMzctOTU0Yi00MTUwLTkxODYtMDNjNTQxYTI2YTE1XkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg" />
+      <Msg name='virat' pic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiAoDwXfflPgO00fa31FNaW4BKAKA9uSB8Zw&usqp=CAU" /> */}
+      {/* {names.map((nm => <Welcome name={nm} />))} */}
+      {users.map((usr) => (
+        <Msg
+          name={usr.name}
+          pic={usr.pic} />
+      ))}
+
     </div>
   );
 }
 
+function Counter() {
+  const [like, setlike] = useState(0)
+  const [dislike, setdislike] = useState(0)
+
+  return (
+    <div>
+      <button onClick={() => setlike(like + 1)}>👍{like}</button>
+      <button onClick={() => setdislike(dislike + 1)}>👎{dislike}</button>
+
+    </div>
+  )
+
+
+}
+function Msg({ pic, name }) {
+  // const name = "Mano"
+  return (
+    <div className='user-container'>
+      <img className='profile-pic'
+        src={pic} alt={name}></img>
+      <h1>Hello {name}</h1>
+      <Counter />
+    </div>
+  )
+}
+
+function Welcome({ name }) {
+  // const { name, pic } = props // object destructuring
+
+  return (
+    <div>
+      {/* <img className='profile-pic'
+        src={pic} alt='mano'></img> */}
+
+      {/* <h1>Hello {name}</h1> */}
+    </div>
+  )
+}
 export default App;
